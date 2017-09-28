51522557aa62ebc9c25266e861b1082ae8ced777
sali189 <sali18@luc.edu>
2017-09-28 15:11:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 
 int sysWrites(int bufferSize, long numberOfWrites) {
 	char buffer[bufferSize];
-	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_RDWR);
+	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_WRONLY);
 	for(int i = 0; i < numberOfWrites; i++) {
 		syscall(SYS_write, fileHandle, &buffer[0], bufferSize);
 	}
@@ -29,7 +29,7 @@ int sysWrites(int bufferSize, long numberOfWrites) {
 }
 int sysReads(int bufferSize, long numberOfReads){
 	char buffer[bufferSize];
-	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_RDWR);
+	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_RDONLY);
 	for(auto i = 0; i<numberOfReads; i++){
 		syscall(SYS_read, fileHandle, &buffer[0], bufferSize);
 	}
