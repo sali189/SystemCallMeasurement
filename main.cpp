7e9e30443d19f8a6a2d87c92e36ce4c3f40402f0
sali189 <sali18@luc.edu>
2017-10-01 13:40:47 -0500

    Update
    Found out how to parse exact arguments to the executable. Now need to work on having the sysRead and sysWrite functions read in a user-specified arguments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * main.cc
+ * main.cpp
  *
  *  Created on: Sep 25, 2017
  *      Author: zain
@@ -8,16 +8,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <iostream>
 #include <sys/types.h>
 #include <assert.h>
+#include <vector>
 #include <time.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
-
 using namespace std;
 
-
+double timespec_to_ms(struct timespec *ts){
+	return ts -> tv_sec*1000.0 + ts->tv_nsec/1000000.0;
+}
 int sysWrites(int bufferSize, long numberOfWrites) {
 	char buffer[bufferSize];
 	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_WRONLY);
@@ -25,7 +27,7 @@ int sysWrites(int bufferSize, long numberOfWrites) {
 		syscall(SYS_write, fileHandle, &buffer[0], bufferSize);
 	}
 	 syscall(SYS_close, fileHandle);
-
+	 return 0;
 }
 int sysReads(int bufferSize, long numberOfReads){
 	char buffer[bufferSize];
@@ -34,21 +36,30 @@ int sysReads(int bufferSize, long numberOfReads){
 		syscall(SYS_read, fileHandle, &buffer[0], bufferSize);
 	}
 	syscall(SYS_close, fileHandle);
+	return 0;
 }
 
 int main(int argc, char* argv[]){
-	clock_t start = clock();
+/*
+	if(argc<7){
+		cerr<<"not enough arguments: " << argv[0] << endl;
+		return 1;
+	}*/
+	int index = 0;
+	for(index = 0; index<argc; index++){
+		if(strncmp(argv[index], "--write_count", 13)==0){
+			printf("main(): Write_count was called\n");
+
+		} else if(strncmp(argv[index],"--read_count", 12)==0){
+			printf("main(): Read_count was called\n");
+			}
+		}
+/*
+	struct timespec start_time, end_time;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
 	int r = sysWrites(1000, 1);
-	clock_t stop = clock();
-	clock_t start2 = clock();
-	int q = sysReads(1000, 1);
-	clock_t stop2 = clock();
-	double elapsedWrite = (double)(stop-start)*1000.0/CLOCKS_PER_SEC;
-	double elapsedRead = (double)(stop2-start2)*1000.0/CLOCKS_PER_SEC;
-	printf("Time to write 50000 with buffer size of 1000: %f \n", elapsedWrite);
-	printf("Time to read 50000 with buffer size of 1000: %f \n", elapsedRead);
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+	printf("Program took %f seconds to finish \n",timespec_to_ms(&end_time)-timespec_to_ms(&start_time));*/
+
 }
-
-
-
 
