2c92ca4ad3b65a2719005cc40c2a0b66dec9ac1f
sali189 <sali18@luc.edu>
2017-10-04 10:32:23 -0500

    Final update 1
    Added the if statements from Louise's code. This isn't exactly the best method, but for the sake of this assignment and there were no given constraints for the layout of the code.

--- main.cpp
+++ main.cpp
@@ -30,9 +30,9 @@ void sysWrites(int bufferSize, long numberOfWrites, const char *FILE) {
 	}
 	 syscall(SYS_close, fileHandle);
 }
-void sysReads(int bufferSize, long numberOfReads){
+void sysReads(int bufferSize, long numberOfReads, const char *FILE){
 	char buffer[bufferSize];
-	int fileHandle = syscall(SYS_open, "file.txt", O_CREAT|O_TRUNC|O_RDWR);
+	int fileHandle = syscall(SYS_open, FILE, O_CREAT|O_TRUNC|O_RDWR);
 	for(auto i = 0; i<numberOfReads; i++){
 		syscall(SYS_read, fileHandle, &buffer[0], bufferSize);
 	}
@@ -40,30 +40,42 @@ void sysReads(int bufferSize, long numberOfReads){
 }
 
 int main(int argc, char* argv[]){
-/*
 	if(argc<7){
 		cerr<<"not enough arguments: " << argv[0] << endl;
 		return 1;
-	}*/
+	}
+
 	struct timespec start_time, end_time;
-	int index = 0;
-	for(index = 0; index<argc; index++){
-		if(strncmp(argv[index], "--write_count", 13)==0){
-			printf("main(): Write_count was called\n");
-			clock_gettime(CLOCK_REALTIME, &start_time);
-				sysWrites(1000000, 10000000, "file.txt");
+	int index=1;
+	if(strncmp(argv[index], "--buffer_size",13)==0) {
+		char* b = argv[index+1]; 
+		long bufferSize;
+		bufferSize= atol(b);
+		if (strncmp(argv[index+2], "--file_name",11)==0) {
+			char* file= argv[index+3];
+			if(strncmp(argv[index+4], "--write_count", 13)==0){
+				clock_gettime(CLOCK_REALTIME, &start_time);
+				char* l = argv[index+5]; 
+				long numberOfWrites;	
+				numberOfWrites= atol(l);		
+				sysWrites(bufferSize, numberOfWrites, file);
+				long total= bufferSize*numberOfWrites;
+				printf("Wrote a total of %ld bytes.\n",total);
 				clock_gettime(CLOCK_REALTIME, &end_time);
 				printf("Program took %f seconds to finish \n",timespec_to_ms(&end_time)-timespec_to_ms(&start_time));
-
-		} else if(strncmp(argv[index],"--read_count", 12)==0){
-			printf("main(): Read_count was called\n");
-			clock_gettime(CLOCK_REALTIME, &start_time);
-			sysReads(100000,1000000);
-			clock_gettime(CLOCK_REALTIME, &end_time);
-			printf("Program took %f seconds to finish \n",timespec_to_ms(&end_time)-timespec_to_ms(&start_time));
+				} else if(strncmp(argv[index+4],"--read_count", 12)==0){
+					clock_gettime(CLOCK_REALTIME, &start_time);
+					char* l = argv[index+5]; 
+					long numberOfReads;	
+					numberOfReads= atol(l);
+					sysReads(bufferSize,numberOfReads, file);
+					long total= bufferSize*numberOfReads;
+					printf("Read a total of %ld bytes.\n",total);
+					clock_gettime(CLOCK_REALTIME, &end_time);
+					printf("Program took %f seconds to finish \n",timespec_to_ms(&end_time)-timespec_to_ms(&start_time));
+					}
 			}
-		}
-
+          }
 }
 
 
